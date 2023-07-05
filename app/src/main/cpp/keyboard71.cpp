d85e03523d0af1b76c9b315f8f93c0ed5f458b01
lurebat <lurebatter@gmail.com>
2023-07-05 15:32:23 +0300

    Initial native

--- app/src/main/cpp/keyboard71.cpp
+++ app/src/main/cpp/keyboard71.cpp
@@ -20,18 +20,18 @@
 #import <string>
 
 #pragma pack(push, 1)
-struct SCShortcuts {
+struct SCShortcut {
     unsigned char field[8];
 };
-struct Moopads {
+struct Moopad {
 unsigned char field[8];
 };
-struct KeyboardPages {
+struct KeyboardPage {
     unsigned char field[8];
 };
 struct GlobalRoenGLInner {
     unsigned char _padding[0xc20];
-    KeyboardPages* keyboardPage;
+    KeyboardPage* keyboardPage;
 };
 
 struct GlobalRoenGL {
@@ -40,35 +40,27 @@ struct GlobalRoenGL {
 
 #pragma pack(pop)
 
-extern "C" {
-    namespace RoenGL {
-        GlobalRoenGL* getGlobalRoenGL();
-    }
-    namespace KeyboardPage {
-        Moopads* getMoopad(KeyboardPages* keyboardPage);
-    }
-    namespace Emkey {
-        namespace SCShortcut {
-            void* makeAction(SCShortcuts* shortcut, std::string* action);
-            void* performAction(SCShortcuts* shortcut, Moopads* moopad);
-        }
-    }
-}
+extern "C" GlobalRoenGL* _ZN6RoenGL15getGlobalRoenGLEv();
+extern "C" Moopad* _ZN12KeyboardPage9getMoopadEv(KeyboardPage* a);
 
 
 
+extern "C" void* _ZN5Emkey10SCShortcut10makeActionERKSs(SCShortcut* a, std::string *b);
+extern "C" void* _ZN5Emkey14SpaceCompleter13performActionERKNS_10SCShortcutE(SCShortcut *a, Moopad *b);
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_lurebat_keyboard71_Native_echo(JNIEnv* env,
                                                  jclass,
                                                  jstring echo) {
-    auto globalRoenGL = RoenGL::getGlobalRoenGL();
-    auto keyboardPage = globalRoenGL->inner->keyboardPage;
-    auto moopad = KeyboardPage::getMoopad(keyboardPage);
 
-    SCShortcuts shortcut = {};
+    auto globalRoenGL = _ZN6RoenGL15getGlobalRoenGLEv();
+    auto keyboardPage = globalRoenGL->inner->keyboardPage;
+    auto moopad = _ZN12KeyboardPage9getMoopadEv(keyboardPage);
+
+    SCShortcut shortcut;
     std::string action = "linebksp";
-    Emkey::SCShortcut::makeAction(&shortcut, &action);
-    Emkey::SCShortcut::performAction(&shortcut, moopad);
+    _ZN5Emkey10SCShortcut10makeActionERKSs(&shortcut, &action);
+    _ZN5Emkey14SpaceCompleter13performActionERKNS_10SCShortcutE(&shortcut, moopad);
+
 
     return echo;
 
